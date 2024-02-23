6ed23859b76c2c3f2ca25afda8e5f09ae89f2a87
Sumesh Kumar Panda <sumeshpanda.k@gmail.com>
2024-02-24 04:26:00 +0530

    initial push

--- app.py
+++ app.py
@@ -4,25 +4,20 @@ import os
 import aws_cdk as cdk
 
 from serverless_api.serverless_api_stack import ServerlessApiStack
+from serverless_api.test_s3_stack import testS3Stack
 
 
 app = cdk.App()
-ServerlessApiStack(app, "ServerlessApiStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
+ServerlessApiStack(
+    app,
+    "ServerlessApiStack",
+    env=cdk.Environment(account="316770681739", region="ap-south-1"),
+)
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
+# testS3Stack(app, "testS3Stack", env=cdk.Environment(account='316770681739', region='us-east-1'),)
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+# Add tags to all resources in the app
+cdk.Tags.of(app).add("source", "cdk-serverless-api")
 
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
 
 app.synth()
