7d1f516a348ebbfdde71e44e0f26d2025f0d1664
Sumesh Kumar Panda <sumeshpanda.k@gmail.com>
2024-02-26 16:56:55 +0530

    Added pipeline resources

--- app.py
+++ app.py
@@ -5,19 +5,30 @@ import aws_cdk as cdk
 
 from serverless_api.serverless_api_stack import ServerlessApiStack
 from serverless_api.test_s3_stack import testS3Stack
-
+from pipeline_stack.pipeline_stack import PipelineStack
+from pipeline_stack.test_stack import CdkCodePipelineStack
+from pipeline_stack.pipeline_from_scratch import PipelineStackFromScratch
 
 app = cdk.App()
+
+# testS3Stack(app, "testS3Stack", env=cdk.Environment(account='316770681739', region='us-east-1'),)
+
 ServerlessApiStack(
     app,
     "ServerlessApiStack",
     env=cdk.Environment(account="316770681739", region="ap-south-1"),
 )
 
-# testS3Stack(app, "testS3Stack", env=cdk.Environment(account='316770681739', region='us-east-1'),)
+
+PipelineStackFromScratch(
+    app,
+    "PipelineStackFromScratch",
+    env=cdk.Environment(account="316770681739", region="ap-south-1"),
+)
+
 
 # Add tags to all resources in the app
-cdk.Tags.of(app).add("source", "cdk-serverless-api")
+cdk.Tags.of(app).add("source", "cdk")
 
 
 app.synth()
